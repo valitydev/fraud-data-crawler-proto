d0ed3b984d2821cba98d8ff53e23e2bb554e879a
ggmaleva <ggmaleva@yandex.ru>
2022-09-09 16:17:12 +0300

    add proto

--- proto/fraud_data_crawler.thrift
+++ proto/fraud_data_crawler.thrift
@@ -2,4 +2,19 @@
 namespace java dev.vality.fraud_data_crawler
 namespace erlang fraud_data_crawler
 
+enum ListType {
+    black
+    white
+    grey
+    naming
+}
 
+/**
+*  Кандидат на добавление в ЧБ списки
+**/
+struct FraudDataCandidate {
+    1: required string type
+    2: required string value
+    3: required ListType list
+    4: required string source
+}
