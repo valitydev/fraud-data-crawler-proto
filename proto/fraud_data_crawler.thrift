5e221d1a205d9f46a5bfc49bef87dcfd96f86ab7
Gregory <32060161+ggmaleva@users.noreply.github.com>
2022-09-12 13:30:27 +0300

    add proto (#1)
    * add proto
    
    * fix
    
    * update shade plugin
    
    * add merchant and shop id
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

--- proto/fraud_data_crawler.thrift
+++ proto/fraud_data_crawler.thrift
@@ -2,4 +2,21 @@
 namespace java dev.vality.fraud_data_crawler
 namespace erlang fraud_data_crawler
 
+enum ListType {
+    black
+    white
+    grey
+    naming
+}
 
+/**
+*  Кандидат на добавление в ЧБ списки
+**/
+struct FraudDataCandidate {
+    1: required string type
+    2: required string value
+    3: required ListType listType
+    4: required string source
+    5: optional string merchantId
+    6: optional string shopId
+}
